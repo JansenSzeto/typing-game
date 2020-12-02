8e21337ecd8d06af65fae066332f134ed590166e
JansenSzeto <66149797+JansenSzeto@users.noreply.github.com>
2020-12-02 17:42:08 +0800

    commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import datetime
 import random
+import translate
 
 pygame.init()
 
@@ -19,23 +20,28 @@ running = True
 key = ""
 words = []
 no_typed = 0
+character = 0
 
 def choose_word():
+	global character
 	global words
 	f = open("words.txt", "r")
 	k = f.readlines()
-	words.append((k[random.randrange(1, 1000)])[:-1])
+	words.append((k[random.randrange(1, 1000)])[:-1]+" ")
 	f.close()
+	character = 0
 for q in range(NUMBEROFWORDS):
 	choose_word()
 
 def show_text():
 	score_to_show = font.render(f"words: {no_typed}", True, (TEXT_COLOR))
 	screen.blit(score_to_show, (20, 20))
+	current = font.render(f"current: {words[0][character]}", True, (TEXT_COLOR))
+	screen.blit(current, (20, 50))
 	word_to_type = font.render(words[0], True, (255, 255, 255))
-	screen.blit(word_to_type, (get_space(" ".join(words)), 270))
-	text = font.render(" ".join(words[1:]), True, (TEXT_COLOR))
-	screen.blit(text, (get_space_after(get_space(" ".join(words))), 270))
+	screen.blit(word_to_type, (get_space("".join(words)), 270))
+	text = font.render("".join(words[1:]), True, (TEXT_COLOR))
+	screen.blit(text, (get_space_after(get_space("".join(words))), 270))
 
 def get_space(a):
     space_between = (SCREENWIDTH-(len(a)*18))//2
@@ -52,10 +58,13 @@ while running:
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_TAB:
 				choose_word()
-			elif event.key == pygame.K_SPACE:
-				words.pop(0)
-				choose_word()
-				no_typed+=1
+			elif translate.translate[event.key] == words[0][character]:
+				if character+1 < len(words[0]):
+					character+=1
+				else:
+					words.pop(0)
+					choose_word()
+					no_typed+=1
 	screen.fill(BACKGROUND_COLOR)
 	show_text()
 	pygame.display.update()
